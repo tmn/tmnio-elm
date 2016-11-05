6e278b518fbe3fb292d79d4cae71777d179aa23b
Tri Nguyen <mail@trimn.net>
2016-11-06 00:12:26 +0100

    Add Routing.elm

---
+++ src/Routing.elm
@@ -0,0 +1,43 @@
+module Routing exposing (..)
+
+import String
+import Navigation
+import UrlParser exposing (..)
+import Blog.Models exposing (BlogPostId)
+
+
+type Route
+    = BlogRoute
+    | BlogPostRoute BlogPostId
+    | NotFoundRoute
+
+
+matchers : Parser (Route -> a) a
+matchers =
+    oneOf
+        [ format BlogRoute (s "")
+        , format BlogPostRoute (s "post" </> int)
+        , format BlogRoute (s "posts")
+        ]
+
+
+hashParser : Navigation.Location -> Result String Route
+hashParser location =
+    location.hash
+        |> String.dropLeft 1
+        |> parse identity matchers
+
+
+parser : Navigation.Parser (Result String Route)
+parser =
+    Navigation.makeParser hashParser
+
+
+routeFromResult : Result String Route -> Route
+routeFromResult result =
+    case result of
+        Ok route ->
+            route
+
+        Err string ->
+            NotFoundRoute
