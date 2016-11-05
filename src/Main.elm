8d2797cabf6a0ccd21f97a52d68caa91cdaefeb6
Tri Nguyen <mail@trimn.net>
2016-11-06 00:17:09 +0100

    Change Html.App.program with Navigation

--- src/Main.elm
+++ src/Main.elm
@@ -1,15 +1,20 @@
 module Main exposing (..)
 
-import Html.App exposing (program)
-import Messages exposing (Msg)
-import Models exposing (Model)
+import Messages exposing (Msg(..))
+import Models exposing (Model, initialModel)
+import Navigation
+import Routing exposing (Route)
 import View exposing (view)
 import Update exposing (update)
 
 
-init : ( Model, Cmd Msg )
-init =
-    ( "tmn.io!", Cmd.none )
+init : Result String Route -> ( Model, Cmd Msg )
+init result =
+    let
+        currentRoute =
+            Routing.routeFromResult result
+    in
+        ( initialModel currentRoute, Cmd.none )
 
 
 subscriptions : Model -> Sub Msg
@@ -17,11 +22,21 @@ subscriptions model =
     Sub.none
 
 
+urlUpdate : Result String Route -> Model -> ( Model, Cmd Msg )
+urlUpdate result model =
+    let
+        currentRoute =
+            Routing.routeFromResult result
+    in
+        ( { model | route = currentRoute }, Cmd.none )
+
+
 main : Program Never
 main =
-    program
+    Navigation.program Routing.parser
         { init = init
         , view = view
         , update = update
+        , urlUpdate = urlUpdate
         , subscriptions = subscriptions
         }
