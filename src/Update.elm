1070483de9c269b9c32be5e85d615f2b16d696fd
Tri Nguyen <mail@trimn.net>
2016-11-06 14:53:39 +0100

    Handle messages in Update.elm

--- src/Update.elm
+++ src/Update.elm
@@ -2,10 +2,15 @@ module Update exposing (..)
 
 import Messages exposing (Msg(..))
 import Models exposing (Model)
+import Blog.Update
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         BlogMsg subMsg ->
-            ( model, Cmd.none )
+            let
+                ( updatedPosts, cmd ) =
+                    Blog.Update.update subMsg model.blogPosts
+            in
+                ( { model | blogPosts = updatedPosts }, Cmd.map BlogMsg cmd )
