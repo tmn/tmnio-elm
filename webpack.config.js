7fea70a3561923f94dc76cd9dd27fcc74ab7fb16
Tri Nguyen <mail@trimn.net>
2016-11-06 14:48:00 +0100

    Remove trailing commans

--- webpack.config.js
+++ webpack.config.js
@@ -24,28 +24,28 @@ module.exports = {
       {
         test:    /\.html$/,
         exclude: /node_modules/,
-        loader:  'file?name=[name].[ext]',
+        loader:  'file?name=[name].[ext]'
       },
       {
         test:    /\.elm$/,
         exclude: [/elm-stuff/, /node_modules/],
-        loader:  'elm-webpack',
+        loader:  'elm-webpack'
       },
       {
         test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'url-loader?limit=10000&mimetype=application/font-woff',
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
       },
       {
         test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'file-loader',
+        loader: 'file-loader'
       },
     ],
 
-    noParse: /\.elm$/,
+    noParse: /\.elm$/
   },
 
   devServer: {
     inline: true,
-    stats: { colors: true },
+    stats: { colors: true }
   }
 };
